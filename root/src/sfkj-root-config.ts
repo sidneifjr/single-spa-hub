fa4fa6101959590013e6ee7a989d25570c34e22f
Sidnei Farias <sidneijr.77@gmail.com>
2025-06-14 01:32:05 -0300

    feat: instala Material UI e estiliza a navbar

--- root/src/sfkj-root-config.ts
+++ root/src/sfkj-root-config.ts
@@ -1,16 +1,6 @@
 import { registerApplication, start, LifeCycles } from "single-spa";
 
 registerApplication({
-  name: "@sfkj/mfe-hello-world",
-  app: () =>
-    import(
-      /* webpackIgnore: true */ // @ts-ignore-next
-      "@sfkj/mfe-hello-world"
-    ),
-  activeWhen: ["/hello-world"],
-});
-
-registerApplication({
   name: "@sfkj/mfe-react-navbar",
   app: () =>
     import(
