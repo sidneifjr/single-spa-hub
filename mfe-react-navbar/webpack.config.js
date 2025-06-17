2ffe0ac80a142e286647bc0e9ef5f3830295b402
Sidnei Farias <sidneijr.77@gmail.com>
2025-06-17 00:27:05 -0300

    fix: handle dispatch.getOwner error by adding React as peerDependency and set it as an external dependency

--- mfe-react-navbar/webpack.config.js
+++ mfe-react-navbar/webpack.config.js
@@ -12,5 +12,11 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
+    externals: [
+      /^react$/,
+      /^react\/.*/,
+      /^react-dom$/,
+      /^react-dom\/.*/
+    ]
   });
 };
