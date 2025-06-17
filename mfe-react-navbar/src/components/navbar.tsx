b7b2f9d3c35f1f04b87db75278752a5103d3e7d9
Sidnei Farias <sidneijr.77@gmail.com>
2025-06-17 01:02:10 -0300

    feat(navbar): adiciona itens, exibidos ao clicar no perfil do usuário

--- mfe-react-navbar/src/components/navbar.tsx
+++ mfe-react-navbar/src/components/navbar.tsx
@@ -6,8 +6,64 @@ import Typography from '@mui/material/Typography';
 import Badge from '@mui/material/Badge';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import NotificationsIcon from '@mui/icons-material/Notifications';
+import { Divider, Menu, MenuItem } from '@mui/material';
+import { useState } from 'react';
 
 export default function Navbar() {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState<null | HTMLElement>(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = 'primary-search-account-menu';
+
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Usuário</MenuItem>
+
+      <Divider />
+
+      <MenuItem onClick={handleMenuClose}>Configurações</MenuItem>
+      <MenuItem onClick={handleMenuClose}>Perfil</MenuItem>
+
+      <Divider />
+
+      <MenuItem onClick={handleMenuClose}>Sair</MenuItem>
+    </Menu>
+  );
+  
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" sx={{ backgroundColor: '#9C27B0' }}>
@@ -24,16 +80,18 @@ export default function Navbar() {
           <Box sx={{ flexGrow: 1 }} />
 
           <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-            <IconButton
-              size="large"
-              edge="end"
-              aria-label="account of current user"
-              aria-haspopup="true"
-              onClick={() => {}}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
+            <MenuItem onClick={handleProfileMenuOpen}>
+              <IconButton
+                size="large"
+                edge="end"
+                aria-label="account of current user"
+                aria-haspopup="true"
+                onClick={() => {}}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+            </MenuItem>
 
             <IconButton
               size="large"
@@ -46,6 +104,8 @@ export default function Navbar() {
           </Box>
         </Toolbar>
       </AppBar>
+
+      {renderMenu}
     </Box>
   );
 }
