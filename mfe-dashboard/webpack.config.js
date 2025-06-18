0fafdac054a0608d78381271a69201513a196be2
Sidnei Farias <sidneijr.77@gmail.com>
2025-06-18 00:58:35 -0300

    fix: atualiza mfe-dashboard

---
+++ mfe-dashboard/webpack.config.js
@@ -0,0 +1,22 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "sfkj",
+    projectName: "mfe-dashboard",
+    webpackConfigEnv,
+    argv,
+    outputSystemJS: false,
+  });
+
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+    externals: [
+      /^react$/,
+      /^react\/.*/,
+      /^react-dom$/,
+      /^react-dom\/.*/
+    ]
+  });
+};
